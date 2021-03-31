81185ea7daec0723e76173afbde68f5dad8609e3
Wade Belden <wade.belden1@gmail.com>
2021-03-30 21:37:05 -0500

    Initial Commit

---
+++ Shared/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  Shared
+//
+//  Created by Wade Belden on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
